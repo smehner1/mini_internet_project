57e4e5f54b9e0b0e449904d57e353a93754acc97
Stefan Mehner <stefan.mehner@b-tu.de>
2022-03-16 15:18:06 +0100

    added startup script for the additional stuff when startup sucessfully finished
    - start ping.py at matrix docker container
    - start upload matrix screen session
    - start upload looking-glass session

---
+++ platform/startup_upload_scripts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+### start ping.py in matrix container
+echo "start ping.py script in matrix docker container"
+sudo docker exec MATRIX tmux new-session -d -s ping 'cd /home; python ping.py'
+echo
+### start matrix upload script in screen
+
+echo "start screen sessions"
+for serv in upload_matrix upload_looking_glass ; do #upload_bgpdumps upload_netflows ; do
+    echo " - start $serv screen session"
+    sudo screen -d -m -S $serv ./utils/$serv.sh 
+done
+echo
+
+echo "running screen sessions"
+echo "-----------------------"
+sudo screen -list
+echo "-----------------------"
+echo
+
+### add cronjob to automatically commit changes in matrix
+
+# we have a cronjob that commits the matrix html page
+# Oliver has a repo running at /scratch/matrix_monitor
+# Stefan also wrote a mini script at `utils/commit_matrix.sh``
+#   ​sudo crobtab -e 
+#  ​0 6 * * * bash /scratch/mini-internet/platform/utils/commit_matrix.sh
+
+
+### delete netflow and bgpdump data that are older than x days
+# find /path/to/files* -mtime +5 -exec rm {} \;
