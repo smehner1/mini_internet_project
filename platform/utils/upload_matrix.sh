721a31b815d36f51a0e073a3ad10f1f1045f6ced
Stefan Mehner <stefan.mehner@b-tu.de>
2022-03-16 14:16:58 +0100

    removed config from git
    utils/upload_matrix.sh
    	matrix.html file from MATRIX docker container is now copied directly to webserver location (/var/www/html/matrix

--- platform/utils/upload_matrix.sh
+++ platform/utils/upload_matrix.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+WEBSERVER_LOCATION="/var/www/html"
+
+mkdir -p $WEBSERVER_LOCATION/matrix
+
 while true
 do
-    docker cp MATRIX:/home/matrix.html matrix.html
-    scp matrix.html thomahol@virt07.ethz.ch:/home/web_commnet/public_html/routing_project/matrix/
+    docker cp MATRIX:/home/matrix.html $WEBSERVER_LOCATION/matrix/index.html
+    
+    # adjust rights
+    chown www-data:www-data $WEBSERVER_LOCATION/matrix/index.html
+    find $WEBSERVER_LOCATION/matrix/ -type d -exec chmod 755 {} \;
+    find $WEBSERVER_LOCATION/matrix/ -type f -exec chmod 644 {} \;
     echo 'matrix sent'
     sleep 10
+
 done
