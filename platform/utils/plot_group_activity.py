b64f5be3d35bba4a3b39341d62be886245438b60
Stefan Mehner <stefan.mehner@b-tu.de>
2022-05-16 11:10:26 +0200

    added group tracking metrics - login count and logged in duration

--- platform/utils/plot_group_activity.py
+++ platform/utils/plot_group_activity.py
@@ -4,9 +4,18 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-csv_file="./stats/user_activity.csv"
+
+csv_file_hist_loc="./stats/group_activity_hist_loc.csv"
+csv_file_login_count="./stats/group_activity_login_count.csv"
+csv_file_login_duration="./stats/group_activity_login_duration.csv"
+output="/var/www/html/stats/group_activity.png"
+
+csv_files= [csv_file_hist_loc, csv_file_login_count, csv_file_login_duration]
+# csv_files=[csv_file_login_duration]
 
 # get group names form AS_config -> all groups with NoConfig
+
+fig, ax = plt.subplots(3, 1, sharex=True, figsize=(10,8))
 
 group_names=['date']
 
@@ -24,13 +33,20 @@ with open('config/AS_config.txt', mode ='r')as file:
         group_names.append(ASN)
 
 print(group_names)
-df = pd.read_csv(csv_file, header=None, sep=" ")
-df.columns=group_names
 
-df['date']=pd.to_datetime(df['date'])
-print(df)
+counter=0
+for cur_csv in csv_files:
+  if counter==0: ylabel="bash hist LOC"
+  elif counter ==1: ylabel="login count"
+  else: ylabel="logged in (min)"
+  df = pd.read_csv(cur_csv, header=None, sep=" ")
+  df.columns=group_names
+  df['date']=pd.to_datetime(df['date'])
+  print(df)
+  df.set_index('date', inplace=True)
+  df.plot.line(y=group_names[1:], ax=ax[counter], legend=False, ylabel=ylabel)
+  counter+=1
 
-df.set_index('date', inplace=True)
-df.plot.line(y=group_names[1:],  title="Group Activity", ylabel="bash history length", ylim=(0,500))
-
-plt.savefig("/var/www/html/stats/group_activity.png")
+ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+          ncol=6, fancybox=True)
+plt.savefig(output)
